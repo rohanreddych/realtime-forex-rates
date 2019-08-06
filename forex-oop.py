021ac2665cf2f8a40b1c134e1acc317765ee9b74
Rohan Reddy Chirasani <43641071+rohanreddych@users.noreply.github.com>
2019-08-06 18:12:33 +0530

    Update forex-oop.py
    Arranged the code into functions

--- forex-oop.py
+++ forex-oop.py
@@ -1,9 +1,9 @@
 from urllib.request import urlopen
 import json
-from secrets import *
+import secrets
 import pandas as pd
 import numpy as np
-apikey = "M0E2RAZRGG836JDA"
+apikey = secrets.apikey
 
 def show_c():
     #list_c = pd.read_csv('list.csv')
@@ -21,29 +21,30 @@ def show_c():
         print(a['Currency'][x] +" " + a['Alphabetic_Code'][x])  
     
     
-def get_rate(fc,tc):
-    #print("Enter three letter currency code, Press 1 if you want to lookup symbol ")
+def get_rate():
+    print("Enter three letter currency code, Press 1 if you want to lookup symbol ")
+    fc = input()
     if fc == "":
         fc = "USD"
-    #fc = input()
-    """try:
+    try:
         if int(fc) == 1:
             show_c()
             fc = input("Enter FROM Currency\n")
     except ValueError:
-        pass"""
+        pass
 
-    """if fc == "":
+    if fc == "":
         print("No FROM currency entered default is USD")
-        fc = "USD"""
+        fc = "USD"
 
-    #tc = input("Enter TO currency \n")
-    """while tc=="":
-        tc = input("TO currency cannot be empty !!!\n")"""
-    """fc = fc.upper()
+    tc = input("Enter TO currency \n")
+    while tc=="":
+        tc = input("TO currency cannot be empty !!!\n")
+     
+    fc = fc.upper()
     fc = fc.strip()
     tc = tc.upper()
-    tc = tc.strip()"""
+    tc = tc.strip()
 
     url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=" +fc+"&to_currency="+tc+"&apikey="+apikey
 
@@ -53,8 +54,8 @@ def get_rate(fc,tc):
             try:
                 a_d = dict(data["Realtime Currency Exchange Rate"])
                 a = pd.DataFrame(data = a_d, index = [0])
-                #print("From"+"\t"+fc+" To"+"\t"+tc+"\nThe Exchange Rate is \t"+a["5. Exchange Rate"][0])
-                #print("One "+fc+" = "+a['5. Exchange Rate'][0] +" "+ tc)
+                print("From"+"\t"+fc+" To"+"\t"+tc+"\nThe Exchange Rate is \t"+a["5. Exchange Rate"][0])
+                print("One "+fc+" = "+a['5. Exchange Rate'][0] +" "+ tc)
                 return a['5. Exchange Rate'][0]
             except KeyError:
                 err = "Please Enter Valid Details"
@@ -64,5 +65,5 @@ def get_rate(fc,tc):
         return err
 
    
-"""if __name__=='__main__':
-    get_rate()"""
+if __name__=='__main__':
+    get_rate()
